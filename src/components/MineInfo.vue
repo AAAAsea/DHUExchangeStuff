02c7994d16a24401b66471f6d71bc7b864ace67b
Asea <569389750@qq.com>
2022-04-23 20:20:07 +0800

    发布post后自动清除、注册昵称密码二次校验，readme

--- src/components/MineInfo.vue
+++ src/components/MineInfo.vue
@@ -1,11 +1,22 @@
 <template>
-  <el-empty description="description" />
+  <div class="acount-info">
+    <ul>
+      <li>
+        <span > 头像</span>
+        <img :src="store.state.data?.user?.headerUrl" alt="">
+      </li>
+      <li>
+        <span > 昵称</span>
+          
+      </li>
+    </ul>
+
+  </div>
 </template>
 
-<script>
-export default {
-
-}
+<script setup>
+import { useStore } from "vuex";
+const store = useStore()
 </script>
 
 <style>
